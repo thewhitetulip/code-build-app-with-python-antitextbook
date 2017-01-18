e238d9224d29b6188e85e834e185a648866d4234
Suraj Patil <sapatil@live.com>
2017-01-18 21:55:04 +0530

    2.2 understanding comments

--- ch2/file.py
+++ ch2/file.py
@@ -1,6 +1,17 @@
-import os
+""" file.py. Author: Someone. Date: Jan 18 2017.
+ prints all the files and folders in the current working directory.
+ execution: python3 file.py " i " ''''''''''''' "".
+ dependency: Python3."""
 
-files = os.listdir()
+'''
+this is a random unrequired multi line comment just to signify that we can use " or """"""""" inside this triple quote
+'''
 
+import os 
+
+files = os.listdir() 
+
+# for loop will print all the txt files in the current working directory.
 for file in files:
-    print(file)
+    if file.endswith(".txt"):
+        print(file)
