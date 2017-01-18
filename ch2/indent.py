da792600fce74e1da5b71d7fd22f41bf9c7aed94
Suraj Patil <sapatil@live.com>
2017-01-18 22:21:05 +0530

    2.3 indentation in Python

---
+++ ch2/indent.py
@@ -0,0 +1,20 @@
+a = 123
+b = 124
+
+if a > 1:
+    print("A is greater than 1")
+    if b < 1256:
+        print("B is less than 1256")
+        print("B is less than 1256")
+        print("B is less than 1256")
+        print("B is less than 1256")
+        print("B is less than 1256")
+        print("B is less than 1256")
+    print("This is a great day!")
+else:
+    print("A is less than or equal to 1")
+
+print("we are out of the if block")
+print("we are out of the if block")
+print("we are out of the if block")
+print("we are out of the if block")
