409c2dccd7d1369a837658b375fc4953bbbb7632
Suraj Patil <sapatil@live.com>
2017-01-19 23:04:20 +0530

    3.2 more info on variables

---
+++ variableop.py
@@ -0,0 +1,37 @@
+'''i = 1
+print(i)
+print(id(i))
+i = "now it is string"
+print(i)
+print(id(i))
+i = 1.1
+print(i)
+print(id(i))
+
+print(type(i) == type(1.1))
+print(type(i) == type("1.1"))
+print(type(i) == type(1))
+'''
+true = 1; false = 2
+true, false = 1,2
+True = 1
+i = True
+j = False
+print(i)
+i = not i
+print(i)
+
+print(i and i)
+'''
+i = 1 # create a variable with value 1.
+i == 1 # compare if the value of i is 1.
+
+#i+, -, +=, -=, /=, *=, *, <, >, <=, >=, ==, =, **, in, not, and, or 
+
+#1 + 1
+#1 - 1
+#i +=1 # i = i + 1
+#i -= 1 # i = i - 1
+i = 12
+i /=2
+print(i)'''
